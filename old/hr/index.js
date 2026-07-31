09e0c17ca556dd9da2b33d1e63eeb0581c8978da
Lerato <lphokomela@chronoefficient.co.za>
2026-07-31 09:04:53 +0200

    Remove obsolete legacy IDP files

--- old/hr/index.js
+++
@@ -1,8 +0,0 @@
-import { idDocumentDefinition } from "./documents/id-document.definition.js";
-import { bankConfirmationDefinition } from "./documents/bank-confirmation.definition.js";
-
-export const hrDocumentRegistry = {
-  [idDocumentDefinition.documentType]: idDocumentDefinition,
-  [bankConfirmationDefinition.documentType]:
-    bankConfirmationDefinition
-};
